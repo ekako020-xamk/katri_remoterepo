ca71b9b57fbd23baa76bb7f6da1fe69279c89ca6
Katri <ekako020@edu.xamk.fi>
2024-02-01 18:44:21 +0200

    virheen selkeytystä, ei toimi vielä

--- index.ts
+++ index.ts
@@ -36,28 +36,27 @@ app.use(express.static(path.resolve(__dirname, "public"))); // public-kansio, jo
 
 app.post("/upload", async (req: express.Request, res: express.Response) => {
 
-    let tiedostonimi : string = req.file?.originalname || "";
+    let tiedostonimi: string = req.file?.originalname || "";
 
     uploadKasittelija(req, res, async (err: any) => {
-
         if (err instanceof multer.MulterError) {
-            res.render("upload", { virhe: "Tiedosto on tiedostokooltaan liian suuri (> 500kt).", teksti: req.body.pp, jsonData: {} });
+            res.render("lisaa", { "virhe" : "Tiedosto on tiedostokooltaan liian suuri (> 500kt).", "teksti" : req.body.teksti });
         } else if (err) {
-            res.render("upload", { virhe: "Väärä tiedostomuoto. Käytä ainoastaan JSON-tiedostoja.", teksti: req.body.pp, jsonData: {} });
+            res.render("lisaa", { "virhe" : "Väärä tiedostomuoto. Käytä ainoastaan json-muotoisia tiedostoja.", "teksti" : req.body.teksti });        
+       
         } else {
+
             if (req.file) {
                 try {
-                    //const filePath = path.resolve(__dirname, "tmp", String(req.file.filename));
                     const jsonData = req.file.buffer.toString("utf-8");
-
                     res.render("upload", { virhe: "", teksti: "", jsonData: JSON.parse(jsonData), tiedostonimi });
                 } catch (error) {
                     console.error(error);
-                    res.render("upload", { virhe: "Virhe tiedoston lukemisessa.", teksti: req.body.pp, jsonData: {}, tiedostonimi  });
+                    res.render("upload", { virhe: "Virhe tiedoston lukemisessa.", jsonData: {}, tiedostonimi });
                     return;
                 }
             } else {
-                res.render("upload", { virhe: "Tiedosto puuttuu.", teksti: req.body.pp, jsonData: {}, tiedostonimi });
+                res.render("upload", { virhe: "Tiedosto puuttuu.", jsonData: {}, tiedostonimi });
             }
         }
     });
@@ -65,8 +64,7 @@ app.post("/upload", async (req: express.Request, res: express.Response) => {
 
 app.get("/upload", async (req: express.Request, res: express.Response) => {
    
-    try {
-        
+    try {        
         const suoritukset = await prisma.suoritus.findMany();
         res.render("upload", { virhe: "", teksti: "", suoritukset: suoritukset, jsonData: {} });
     } catch (error) {
