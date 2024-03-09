72ea283a610f7360210c141c8663282b9bbb9b76
ekako020_xamk <112950212+ekako020-xamk@users.noreply.github.com>
2024-03-09 11:42:30 +0200

    Delete prisma directory

--- prisma/migrations/20240126160143_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "suoritus" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "pp" INTEGER NOT NULL,
-    "kk" INTEGER NOT NULL,
-    "vvvv" INTEGER NOT NULL,
-    "askeleet" INTEGER NOT NULL
-);
